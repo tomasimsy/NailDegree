0aad5099fbd71506576404e2ab1a6b78f7a1fa31
Admin <TomAdmin@macs-mbp.lan>
2026-06-14 02:20:06 -0400

    Updated project changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
