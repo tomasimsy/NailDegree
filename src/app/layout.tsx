180a781e6035cd1961b4ebacb8986f988f8047ba
Admin <TomAdmin@macs-mbp.lan>
2026-06-15 00:27:10 -0400

    Updated project changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+  <link rel="manifest" href="/manifest.json" />
+</head>
+
       <body>{children}</body>
     </html>
   )
