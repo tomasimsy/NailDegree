75e79ebbda052a0758f63f7ad9b0192f5d3dfc21
Admin <TomAdmin@macs-mbp.lan>
2026-06-16 20:43:32 -0400

    update 1

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -24,6 +24,7 @@ export default function manifest(): MetadataRoute.Manifest {
       },
       {
         src: '/icons/icon-512x512.png',
+        
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
